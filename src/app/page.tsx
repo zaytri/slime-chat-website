5426d5344a0d374c107d9dba73e5ff48002ab6b3
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-04-03 01:31:40 -0700

    Add GitHub link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,28 @@
 import Button, { ButtonText } from '@/components/button'
 
+const links = [
+  [
+    'Getting Started',
+    'https://forums.slime.chat/resources/getting-started-with-slimechat.3/',
+  ],
+  ['Community Forums', 'https://forums.slime.chat/'],
+  ['Get Themes', 'https://forums.slime.chat/resources/categories/themes.2/'],
+  ['GitHub Source Code', 'https://github.com/zaytri/slime-chat'],
+]
+
 export default function Home() {
   return (
     <div className='space-y-5'>
       <h1 className='mb-7 text-center font-round text-3xl font-medium'>
         Welcome to SlimeChat!
       </h1>
-      <Button
-        className='px-5 pb-3 pt-5'
-        href='https://forums.slime.chat/resources/getting-started-with-slimechat.3/'
-      >
-        <ButtonText className='text-3xl'>Getting Started</ButtonText>
-      </Button>
-
-      <Button className=' px-5 pb-3 pt-5' href='https://forums.slime.chat/'>
-        <ButtonText className='text-3xl'>Community Forums</ButtonText>
-      </Button>
-
-      <Button
-        className=' px-5 pb-3 pt-5'
-        href='https://forums.slime.chat/resources/categories/themes.2/'
-      >
-        <ButtonText className='text-3xl'>Get Themes</ButtonText>
-      </Button>
+      {links.map(([name, path]) => {
+        return (
+          <Button key={name} className='px-5 pb-3 pt-5' href={path}>
+            <ButtonText className='text-3xl'>{name}</ButtonText>
+          </Button>
+        )
+      })}
     </div>
   )
 }
