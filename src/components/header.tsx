f7aa063a274bc781598448a491218328cb38b561
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-09-10 20:23:16 -0700

    Update logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,11 @@ export default function Header() {
   return (
     <header className='flex w-screen flex-col items-center bg-emerald-800 pt-10'>
       <Link href='/'>
-        <Image src={logo} className='logo pb-5' alt='Slime2 Logo' />
+        <Image
+          src={logo}
+          className='logo max-h-48 w-auto px-10 pb-5'
+          alt='Slime2 Logo'
+        />
       </Link>
       <Navigation />
     </header>
