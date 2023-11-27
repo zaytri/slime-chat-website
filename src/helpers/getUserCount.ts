a180cf45f30942d6af8ade46f34ebd7bdddd9c81
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-11-27 14:02:25 -0800

    don't need to use react's cache because there's no parameters

--- src/helpers/getUserCount.ts
+++ src/helpers/getUserCount.ts
@@ -1,8 +1,7 @@
-import { cache } from 'react'
 import prisma from '@/helpers/database'
 
 export const revalidate = 300 // revalidate the data at most every 5 minutes
 
-export const getUserCount = cache(async () => {
+export async function getUserCount() {
   return prisma.user.count()
-})
+}
