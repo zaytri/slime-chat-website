6fe51c41c9d8eaa39bb8dabe7ac37cb45111f2b1
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-12-04 23:08:50 -0800

    update footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,22 +3,25 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className='mt-5 w-screen bg-emerald-800 p-3 text-center text-lime-100'>
-      slime2 created by{' '}
-      <Link
-        href='https://zaytri.com/'
-        className='text-lime-300 hover:underline'
-        target='_blank'
-      >
-        Zaytri
-      </Link>{' '}
-      |{' '}
-      <Link href='/privacy' className='text-lime-300 hover:underline'>
-        Privacy Policy
-      </Link>{' '}
-      |{' '}
-      <Link href='/tos' className='text-lime-300 hover:underline'>
-        Terms of Service
-      </Link>
+      <p>
+        slime2 created by{' '}
+        <Link
+          href='https://zaytri.com/'
+          className='text-lime-300 hover:underline'
+          target='_blank'
+        >
+          Zaytri
+        </Link>
+      </p>
+      <p>
+        <Link href='/privacy' className='text-lime-300 hover:underline'>
+          Privacy Policy
+        </Link>{' '}
+        -{' '}
+        <Link href='/tos' className='text-lime-300 hover:underline'>
+          Terms of Service
+        </Link>
+      </p>
     </footer>
   )
 }
