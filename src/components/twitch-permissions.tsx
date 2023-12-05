d604096995d25564da46900a33327c52f87a6ff7
Zaytri <9072119+zaytri@users.noreply.github.com>
2023-12-04 22:04:19 -0800

    update twitch permissions

--- src/components/twitch-permissions.tsx
+++ src/components/twitch-permissions.tsx
@@ -3,37 +3,29 @@ import Permission from './permission'
 export default function TwitchPermissions() {
   return (
     <section className='mt-10 rounded-lg border-2 border-emerald-900 bg-emerald-700 p-5 text-lime-100'>
-      <h2 className='mb-5 font-round text-2xl font-medium'>
+      <h2 className='mb-5 font-round text-3xl font-medium'>
         Permissions Explanation
       </h2>
       <div className='space-y-3'>
         <Permission title='View stream chat messages'>
           <ul className='ml-4 list-outside list-disc'>
-            <li>{`Used to get your stream's chat messages`}</li>
+            <li>Allows widgets to get the chat messages of your stream</li>
           </ul>
         </Permission>
         <Permission title='View list of followers'>
-          <ul className='ml-4 list-outside list-disc'>
+          <ul className='ml-4 flex list-outside list-disc flex-col gap-2'>
+            <li>Allows widgets to get the follow date of your followers</li>
             <li>
-              Used to get the follow date of a follower that has sent a chat
-              message
-            </li>
-            <li>
-              You can use this to filter or customize chat based on that follow
-              date, such as only showing chat from users who have been following
-              you for over 24 hours
+              This can be used in chat widgets to filter or customize chat based
+              on follow date
             </li>
           </ul>
         </Permission>
         <Permission title='View channel point custom rewards'>
-          <ul className='ml-4 list-outside list-disc'>
+          <ul className='ml-4 flex list-outside list-disc flex-col gap-2'>
             <li>
-              Used to get the data of a channel point redemption that requires
-              viewers to enter text
-            </li>
-            <li>
-              You can use this to filter or customize chat based on channel
-              point redemptions
+              Allows widgets to get the data of redeemed channel point rewards
+              that require text
             </li>
           </ul>
         </Permission>
